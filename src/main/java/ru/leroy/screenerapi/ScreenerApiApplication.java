726c6c6d095f01ba543dd0738d3bcba9ff5b6f1b
Ivan Ivanchuk <clicker.heroes.acg@gmail.com>
2022-09-08 02:56:24 +0300

    Spaces removed
    Signed-off-by: Ivan <clicker.heroes.acg@gmail.com>

--- src/main/java/ru/leroy/screenerapi/ScreenerApiApplication.java
+++ src/main/java/ru/leroy/screenerapi/ScreenerApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ScreenerApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ScreenerApiApplication.class, args);
     }
-
 }
