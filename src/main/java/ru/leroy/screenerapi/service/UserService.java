c3136984230ac99a15dd1f1317f18d915b359b54
Ivan <clicker.heroes.acg@gmail.com>
2022-09-07 21:46:10 +0300

    #2-user-service updatePasswordById, switchUserRateById

--- src/main/java/ru/leroy/screenerapi/service/UserService.java
+++ src/main/java/ru/leroy/screenerapi/service/UserService.java
@@ -1,5 +1,6 @@
 package ru.leroy.screenerapi.service;
 
+import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
 import ru.leroy.screenerapi.entity.UserEntity;
 import ru.leroy.screenerapi.exception.EmailExistException;
@@ -17,9 +18,7 @@ public class UserService {
     }
 
     public UserEntity userById(final Long id) throws UserNotFoundException {
-        return this.repository
-            .findById(id)
-            .orElseThrow(UserNotFoundException::new);
+        return userBy(id);
     }
 
     public UserEntity registration(final UserEntity user) throws EmailExistException {
@@ -32,5 +31,22 @@ public class UserService {
                 }, () -> ref.set(this.repository.save(user))
             );
         return ref.get();
+    }
+
+    public UserEntity updateUserPasswordById(final Long id, final String pass) throws UserNotFoundException {
+        UserEntity updated = userBy(id);
+        updated.setPassword(pass);
+        return repository.save(updated);
+    }
+
+    public UserEntity switchUserRateById(final Long id, final String rate) throws UserNotFoundException {
+        UserEntity updated = userBy(id);
+        updated.setRate(rate);
+        return repository.save(updated);
+    }
+
+
+    private UserEntity userBy(Long id) throws UserNotFoundException {
+        return this.repository.findById(id).orElseThrow(UserNotFoundException::new);
     }
 }
