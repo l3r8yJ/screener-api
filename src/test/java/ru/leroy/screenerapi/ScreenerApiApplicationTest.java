296d8feba10d3480b3f1067f6faaf6463debac4d
Ivan <clicker.heroes.acg@gmail.com>
2022-09-09 23:07:11 +0300

    fix
    Signed-off-by: Ivan <clicker.heroes.acg@gmail.com>

--- src/test/java/ru/leroy/screenerapi/ScreenerApiApplicationTest.java
+++ src/test/java/ru/leroy/screenerapi/ScreenerApiApplicationTest.java
@@ -1,15 +1,9 @@
 package ru.leroy.screenerapi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
 class ScreenerApiApplicationTest {
-
-    @Test
-    void contextLoads() {
-
-    }
 
 }
