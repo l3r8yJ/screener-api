7953de03d8f1af899ddf11507d4f2975eed0c422
Ivan <clicker.heroes.acg@gmail.com>
2022-09-12 02:40:54 +0300

    #67-dry-in-usercontroller fixed routes, dry fixed
    Signed-off-by: Ivan <clicker.heroes.acg@gmail.com>

--- src/test/java/ru/leroy/screenerapi/controller/UserControllerTest.java
+++ src/test/java/ru/leroy/screenerapi/controller/UserControllerTest.java
@@ -226,7 +226,7 @@ class UserControllerTest {
     given(this.service.updateRateById(this.userEntity.getId(), "pro"))
         .willReturn(this.userEntity);
     final MockHttpServletResponse response = this.mvc.perform(
-            put("/user/change-rate/".concat(String.valueOf(this.userEntity.getId())))
+            put("/user/rate/change/".concat(String.valueOf(this.userEntity.getId())))
                 .content(this.userJson.write(this.userEntity).getJson())
                 .contentType(MediaType.APPLICATION_JSON)
         )
@@ -242,7 +242,7 @@ class UserControllerTest {
     given(this.service.updateRateById(this.userEntity.getId(), "pro"))
         .willThrow(UserNotFoundException.class);
     final MockHttpServletResponse response = this.mvc.perform(
-            put("/user/change-rate/".concat(String.valueOf(this.userEntity.getId())))
+            put("/user/rate/change/".concat(String.valueOf(this.userEntity.getId())))
                 .content(this.userJson.write(this.userEntity).getJson())
                 .contentType(MediaType.APPLICATION_JSON)
         )
@@ -258,7 +258,7 @@ class UserControllerTest {
     given(this.service.updateRateById(this.userEntity.getId(), "pro"))
         .willThrow(IllegalStateException.class);
     final MockHttpServletResponse response = this.mvc.perform(
-            put("/user/change-rate/".concat(String.valueOf(this.userEntity.getId())))
+            put("/user/rate/change/".concat(String.valueOf(this.userEntity.getId())))
                 .content(this.userJson.write(this.userEntity).getJson())
                 .contentType(MediaType.APPLICATION_JSON)
         )
