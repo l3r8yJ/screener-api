79f94e94430d85ca2101e943e0723dff0e7bd65e
Ivan <clicker.heroes.acg@gmail.com>
2022-09-12 14:18:51 +0300

    #76-userregistrationdto info dto integration
    Signed-off-by: Ivan <clicker.heroes.acg@gmail.com>

---
+++ src/main/java/ru/leroy/screenerapi/dto/user/info/UserResponseDto.java
@@ -0,0 +1,10 @@
+package ru.leroy.screenerapi.dto.user.info;
+
+import ru.leroy.screenerapi.dto.user.registration.UserResponseRegistration;
+
+/**
+* Full info DTO.
+*/
+public class UserResponseDto extends UserResponseRegistration {
+
+}
